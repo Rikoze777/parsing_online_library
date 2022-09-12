4e6a677ef7a949fab29e0f7a77d0349b6a23ac19
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-09-12 22:08:34 +0300

    add check for redirect

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import os
 import requests
 
 
+def check_for_redirect(response):
+    if response.history == []:
+        pass
+    else:
+        raise requests.exceptions.HTTPError('main')
+
+
 def get_books(book_path, books_count):
     requests.packages.urllib3.disable_warnings(
         requests.packages.urllib3.exceptions.InsecureRequestWarning)
@@ -10,15 +17,21 @@ def get_books(book_path, books_count):
         url = f"https://tululu.org/txt.php?id={number_id}"
         response = requests.get(url, verify=False)
         response.raise_for_status()
-        filename = f"id{number_id}.txt"
-        file_path = os.path.join(book_path, filename)
-        with open(file_path, 'w') as book:
-            book.write(response.text)
+        try:
+            check_for_redirect(response)
+            filename = f"id{number_id}.txt"
+            file_path = os.path.join(book_path, filename)
+            with open(file_path, 'w') as book:
+                book.write(response.text)
+        except requests.exceptions.HTTPError:
+            print("wrong url")
+
 
 def main():
     book_path = 'books'
     books_count = 10
     get_books(book_path, books_count)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
