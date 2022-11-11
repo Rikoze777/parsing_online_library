57335692ee3774628267c72e4824fabef0eca587
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-11-11 18:01:17 +0300

    change to select parser

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import time
+from collections import OrderedDict
 import json
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
@@ -13,27 +14,34 @@ collections.Callable = collections.abc.Callable
 def parse_book_page(response):
     url = response.url
     soup = BeautifulSoup(response.text, 'lxml')
-    soup_text = soup.find('body').find('h1')
-    content_text = soup_text.text
+
+    header_selector = ".ow_px_td h1"
+    header_content = soup.select_one(header_selector)
+    content_text = header_content.text
     content = [" ".join(text.split()) for text in content_text.split('::')]
-    title, author = content
-    title = sanitize_filename(title)
-    author = sanitize_filename(author)
-    soup_genres = soup.find_all('span', class_='d_book')
-    genres = [genre.text for genre in soup_genres]
-    book_genres = ":".join(genres).lstrip('Жанр книги:').strip()
-    soup_img = soup.find(class_='bookimage').find('img')['src']
-    image_url = urljoin(url, soup_img)
-    soup_comments = soup.find_all(class_='texts')
-    decoded_comments = [comment.text for comment in soup_comments]
+    book_title, book_author = content
+    book_title = sanitize_filename(book_title)
+    book_author = sanitize_filename(book_author)
+
+    genres_selector = "span.d_book a"
+    genres_content = soup.select(genres_selector)
+    genres = [genre.string for genre in genres_content]
+    book_genres = " : ".join(genres).lstrip('Жанр книги:').strip()
+
+    image_selector = "div.bookimage img"
+    image_content = soup.select_one(image_selector)['src']
+    image_url = urljoin(url, image_content)
+
+    comments_selector = ".texts span.black"
+    comments_content = soup.select(comments_selector)
+    decoded_comments = [comment.string for comment in comments_content]
     raw_comments = []
     for comment in decoded_comments:
-        comment = comment.split(')')
         raw_comments.append(comment[-1])
-    comments = "\n".join(raw_comments)
+    comments = "\n".join(decoded_comments)
     book_page = {
-                 "title": title,
-                 "author": author,
+                 "title": book_title,
+                 "author": book_author,
                  "genres": book_genres,
                  "image_url": image_url,
                  "comments": comments
@@ -94,36 +102,36 @@ def main():
             try:
                 fantastic_url = f"https://tululu.org/l55/{page}"
                 download_url = "https://tululu.org/txt.php"
+                url = "https://tululu.org/"
                 response = requests.get(fantastic_url, verify=False)
                 response.raise_for_status()
                 soup = BeautifulSoup(response.text, 'lxml')
-                book_tags = soup.find_all(class_="bookimage")
-                for books in book_tags:
-                    for book in books:
-                        url = "https://tululu.org/"
-                        book_tag = book["href"]
-                        book_number = int(str(book_tag)[2:-1])
-                        book_url = urljoin(url, book_tag)
-                        params = {
-                                "id": book_number,
-                        }
-                        book_response = requests.get(book_url, verify=False)
-                        book_response.raise_for_status()
-                        encoded_params = urlencode(params)
-                        download_response = requests.get(download_url,
-                                                         params=encoded_params,
-                                                         verify=False)
-                        download_response.raise_for_status()
-                        check_for_redirect(book_response)
-                        check_for_redirect(download_response)
-                        book_page = parse_book_page(book_response)
-                        download_txt(book_page['title'],
-                                     download_response, txt_folder)
-                        if book_page['comments']:
-                            fetch_comments(book_page['comments'],
-                                           book_number, comments_folder)
-                        download_image(book_page['image_url'], image_folder)
-                        books_dump.append(book_page)
+                selector = ".d_book a[href^='/b']"
+                links = [content["href"] for content in soup.select(selector)]
+                page_links = list(OrderedDict.fromkeys(links))
+                for link in page_links:
+                    book_number = int(str(link)[2:-1])
+                    book_url = urljoin(url, link)
+                    params = {
+                            "id": book_number,
+                    }
+                    book_response = requests.get(book_url, verify=False)
+                    book_response.raise_for_status()
+                    encoded_params = urlencode(params)
+                    download_response = requests.get(download_url,
+                                                     params=encoded_params,
+                                                     verify=False)
+                    download_response.raise_for_status()
+                    check_for_redirect(book_response)
+                    check_for_redirect(download_response)
+                    book_page = parse_book_page(book_response)
+                    download_txt(book_page['title'],
+                                 download_response, txt_folder)
+                    if book_page['comments']:
+                        fetch_comments(book_page['comments'],
+                                       book_number, comments_folder)
+                    download_image(book_page['image_url'], image_folder)
+                    books_dump.append(book_page)
             except requests.exceptions.HTTPError:
                 print("Wrong url")
             except requests.exceptions.ConnectionError:
