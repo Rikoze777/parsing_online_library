1b3d5932d535fb4f122e4aa793a2751cfb9de489
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-11-24 11:24:41 +0300

    fix requirements and fix magic numbers

--- render_website.py
+++ render_website.py
@@ -16,11 +16,13 @@ def on_load():
         books_json = my_file.read()
 
     books = json.loads(books_json)
-    books_chunked = list(chunked(books, 10))
+    books_per_page = 10
+    books_chunked = list(chunked(books, books_per_page))
     pages_count = len(books_chunked)
     os.makedirs('pages/', exist_ok=True)
     for count, chunk in enumerate(books_chunked, 1):
-        page_chunk = list(chunked(chunk, 2))
+        page_rows = 2
+        page_chunk = list(chunked(chunk, page_rows))
         rendered_page = template.render(books=page_chunk,
                                         pages=pages_count,
                                         count=count)
